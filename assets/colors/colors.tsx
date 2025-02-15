cc7a252b6e8cbab0b8563e467f8a76a2beacdd21
Aleksandra Sawicka <aleksandra.sawicka@growbots.com>
2025-02-15 11:11:56 +0100

    change icons

---
+++ assets/colors/colors.tsx
@@ -0,0 +1,8 @@
+export const Colors = {
+  yellowMain: "#E7AC07",
+  yellowSecondary: "#E89506",
+  yellowBackground: "#FFF6DE",
+  white: "#FFFEFC",
+  black: "#333333",
+  blackSecondary: "#707070",
+};
