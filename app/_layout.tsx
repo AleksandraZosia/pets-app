d27a4bb8bb547f44b7c8e43d9b0107449f85b5ef
Aleksandra Sawicka <aleksandra.sawicka@growbots.com>
2025-02-16 11:04:28 +0100

    add fonts and use them in Header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,27 @@
+import React from "react";
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    "Lalezar-Regular": require("@/assets/fonts/Lalezar-Regular.ttf"),
+    "Roboto-Regular": require("@/assets/fonts/Roboto-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  if (error) {
+    return null;
+  }
+  if (fontsLoaded) {
+    SplashScreen.hideAsync();
+  }
+
   return (
     <Stack
       screenOptions={{
