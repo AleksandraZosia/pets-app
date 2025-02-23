b8073c06685a87e6b6d7f0e370a13ec28a2628c0
Aleksandra Sawicka <aleksandra.sawicka@growbots.com>
2025-02-23 12:52:22 +0100

    add calendar

---
+++ components/calendar/mocks.ts
@@ -0,0 +1,10 @@
+export const mockAgenda = {
+  "2025-02-23": [
+    { title: "Item 1", notes: "Notes", height: 100, day: "2025-02-23" },
+    { title: "Item 2", notes: "Notes", height: 100, day: "2025-02-23" },
+  ],
+  "2025-02-24": [
+    { title: "Item 3", notes: "Notes", height: 100, day: "2025-02-24" },
+    { title: "Item 4", notes: "Notes", height: 100, day: "2025-02-24" },
+  ],
+};
