e17cfcfd2e0e6007b9fca4920dbff67fb60e424d
Aleksandra Sawicka <aleksandra.sawicka@growbots.com>
2025-02-23 12:09:41 +0100

    move colors to the right folder, use snakeCase to name consts and update exports - imports

--- assets/colors/colors.tsx
+++
@@ -1,8 +0,0 @@
-export const Colors = {
-  yellowMain: "#E7AC07",
-  yellowSecondary: "#E89506",
-  yellowBackground: "#FFF6DE",
-  white: "#FFFEFC",
-  black: "#333333",
-  blackSecondary: "#707070",
-};
