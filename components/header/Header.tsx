2bf5ed33172728fa8eae296d47be179378af5c9a
Aleksandra Sawicka <aleksandra.sawicka@growbots.com>
2025-02-28 16:23:49 +0100

    modify header to be more universal

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,28 +1,75 @@
-import { View, Text } from "react-native";
-import { Stack } from "expo-router";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { Stack, router } from "expo-router";
 import { COLORS } from "@/consts";
 
 export const Header = ({
   title,
   canGoBack,
+  HeaderRightTitle,
+  handleHeaderRightPress,
 }: {
   title: string;
   canGoBack?: boolean;
+  HeaderRightTitle?: string;
+  handleHeaderRightPress?: () => void;
 }) => {
   return (
     <Stack.Screen
       options={{
         headerShown: true,
-        headerTitle: title,
-        headerTitleStyle: {
-          color: COLORS.BLACK,
-          fontWeight: 400,
-          fontSize: 24,
-          fontFamily: "Lalezar-Regular",
-        },
-        headerStyle: { backgroundColor: COLORS.YELLOW_BACKGROUND },
-        headerBackVisible: !!canGoBack,
+        header: () => (
+          <View
+            style={{
+              backgroundColor: COLORS.YELLOW_BACKGROUND,
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingVertical: 21,
+              paddingHorizontal: 12,
+              shadowColor: COLORS.BLACK,
+              shadowOffset: { width: 0, height: 2 },
+              shadowOpacity: 0.2,
+              shadowRadius: 8,
+              elevation: 8,
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 3,
+                alignContent: "center",
+              }}
+            >
+              {canGoBack && (
+                <Pressable
+                  onPress={() => router.back()}
+                  hitSlop={{ top: 14, bottom: 14, left: 14, right: 14 }}
+                >
+                  <Text style={styles.text}>{`<`}</Text>
+                </Pressable>
+              )}
+
+              <Text style={styles.text}>{title}</Text>
+            </View>
+            {HeaderRightTitle ? (
+              <Pressable onPress={handleHeaderRightPress}>
+                <Text style={styles.text}>{HeaderRightTitle}</Text>
+              </Pressable>
+            ) : null}
+          </View>
+        ),
       }}
     />
   );
 };
+
+const styles = StyleSheet.create({
+  text: {
+    color: COLORS.BLACK,
+    fontWeight: 400,
+    fontSize: 24,
+    fontFamily: "Lalezar-Regular",
+    letterSpacing: 0,
+  },
+});
