e06fd0af2f67ce10344d7a8692cc91e12bac9d00
Aleksandra Sawicka <aleksandra.sawicka@growbots.com>
2025-03-02 09:54:58 +0100

    customize modal

---
+++ components/modal/Modal.tsx
@@ -0,0 +1,65 @@
+import { View, Text, Modal, Pressable, Alert, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { COLORS } from "@/consts";
+
+interface ModalProps {
+  visible?: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+export const CustomModal = ({ visible, onClose, children }: ModalProps) => {
+  return (
+    <>
+      {visible && <View style={styles.overlay} />}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={visible}
+        onRequestClose={() => {
+          onClose();
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>{children}</View>
+        </View>
+      </Modal>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  overlay: {
+    position: "absolute",
+    flex: 1,
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: 1000,
+    backgroundColor: COLORS.BLACK,
+    opacity: 0.5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginHorizontal: 16,
+  },
+  modalView: {
+    backgroundColor: COLORS.WHITE,
+    borderRadius: 2,
+    padding: 24,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+});
